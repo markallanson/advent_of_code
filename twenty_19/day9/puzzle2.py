537998c31b3a4d07faa12a90b59c6acb32b23d77
Mark Allanson <mark@markallanson.net>
2019-12-09 16:23:25 +0000

    2019 Day 9, Puzzle 2.

---
+++ twenty_19/day9/puzzle2.py
@@ -0,0 +1,7 @@
+from twenty_19.day9 import input
+from twenty_19.util import IntCodeComputer
+from twenty_19.util import IOBuffer
+
+io = IOBuffer("io1")
+io.write(2)
+IntCodeComputer(input.boost_program, input_func=io.read).compute()

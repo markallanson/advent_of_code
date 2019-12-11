9331a2fe3f3664d8b7ece487dc14d03f3778d672
Mark Allanson <mark@allanson.org>
2019-12-11 20:13:05 +0000

    2019 Day 11, Puzzle 1

--- twenty_19/day8/__init__.py
+++ twenty_19/day8/__init__.py
@@ -4,3 +4,25 @@ def get_layers(image_data, width, height):
     pixels_per_layer = width * height
     for i in range(0, len(image_data), pixels_per_layer):
         yield image_data[i:i + pixels_per_layer]
+
+
+def merge(layers, width, height):
+    merged_image = [0] * (width * height)
+    #iterate layers in reverse, only take the pixel in each layer if its not transparent
+    for layer in reversed(layers):
+        for pixel_index in range(0, len(layer)):
+            if layer[pixel_index] != 2:
+                merged_image[pixel_index] = layer[pixel_index]
+    return merged_image
+
+def print_image(image, width, height):
+    for y in range(0, height):
+        scan_line = []
+        for x in range(0, width):
+            pixel_index = (y * width) + x
+            pixel = image[pixel_index]
+            if pixel == 1:
+                scan_line.append("█")
+            else:
+                scan_line.append(" ")
+        print("".join(scan_line))
--- twenty_19/day8/puzzle2.py
+++ twenty_19/day8/puzzle2.py
@@ -1,26 +1,5 @@
 from twenty_19.day8 import input
-from twenty_19.day8 import get_layers
-
-def merge(layers, width, height):
-    merged_image = [0] * (width * height)
-    #iterate layers in reverse, only take the pixel in each layer if its not transparent
-    for layer in reversed(layers):
-        for pixel_index in range(0, len(layer)):
-            if layer[pixel_index] != 2:
-                merged_image[pixel_index] = layer[pixel_index]
-    return merged_image
-
-def print_image(image, width, height):
-    for y in range(0, height):
-        scan_line = []
-        for x in range(0, width):
-            pixel_index = (y * width) + x
-            pixel = image[pixel_index]
-            if pixel == 1:
-                scan_line.append("█")
-            else:
-                scan_line.append(" ")
-        print("".join(scan_line))
+from twenty_19.day8 import get_layers, merge, print_image
 
 layers = list(get_layers(input.image, 25, 6))
 final_image = merge(layers, 25, 6)
